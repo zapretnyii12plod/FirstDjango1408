80fcb55b627a6fd71b36ac2a3c386bc54df81a9e
Denis <=>
2023-08-17 21:07:34 +0300

    Migration

--- MainApp/models.py
+++ MainApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+   name  = models.CharField(max_length=100)
+   brand = models.CharField(max_length=100)
+   count = models.PositiveIntegerField() 
+
